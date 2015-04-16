dcfaeac70de7f17af91b6a52b1537724097afb3d
Almas Akchabayev <almas.akchabayev@gmail.com>
2015-04-17 03:21:27 +0600

    deleted delete method from JdbcAddressDao

--- src/main/java/com/epam/aa/sportfinder/dao/jdbc/JdbcAddressDao.java
+++ src/main/java/com/epam/aa/sportfinder/dao/jdbc/JdbcAddressDao.java
@@ -17,10 +17,6 @@ public class JdbcAddressDao extends JdbcBaseDao<Address> implements AddressDao {
             "SELECT id, uuid, deleted, country, city, addressLine1, addressLine2, zipcode " +
                     "FROM Address WHERE id = ?";
 
-    private static final String SQL_DELETE =
-            "UPDATE Address SET deleted = TRUE WHERE id = ?";
-
-
     public JdbcAddressDao(Connection connection) {
         super(connection);
     }
@@ -41,26 +37,6 @@ public class JdbcAddressDao extends JdbcBaseDao<Address> implements AddressDao {
         }
         return address;
     }
-
-//    @Override
-//    public void delete(Address address) {
-//        if (address.getId() == null) {
-//            throw new DaoException(new IllegalArgumentException(
-//                    "Address without id cannot be deleted"));
-//        }
-//
-//        try (PreparedStatement pst = getConnection().prepareStatement(SQL_DELETE)){
-//            pst.setInt(1, address.getId());
-//
-//            int affectedRows = pst.executeUpdate();
-//            if (affectedRows == 0) {
-//                throw new DaoException("Update failed, no rows affected.");
-//            }
-//            address.setDeleted(true);
-//        } catch (SQLException e) {
-//            throw new DaoException("Deleting address failed", e);
-//        }
-//    }
 
     private static Address map(ResultSet resultSet) throws SQLException {
         Address address = new Address();
