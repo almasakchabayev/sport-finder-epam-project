279dd0943051e6e41c89eceff95eb7209697eb28
Almas Akchabayev <almas.akchabayev@gmail.com>
2015-04-17 22:59:13 +0600

    eleted datasource setup in AbstractTest

--- src/main/java/com/epam/aa/sportfinder/dao/DaoManager.java
+++ src/main/java/com/epam/aa/sportfinder/dao/DaoManager.java
@@ -14,4 +14,5 @@ public interface DaoManager {
 
     // TODO:generic getter
 
+
 }
--- src/test/java/com/epam/aa/sportfinder/dao/jdbc/GlobalTestDataSource.java
+++ src/test/java/com/epam/aa/sportfinder/dao/jdbc/GlobalTestDataSource.java
@@ -9,26 +9,6 @@ import javax.sql.DataSource;
 
 public abstract class GlobalTestDataSource {
     public static final DaoFactory DAO_FACTORY = DaoFactory.getInstance(DaoFactory.Type.JDBC);
-//    private static final DataSource dataSource = initDataSource();
-
-//    private static DataSource initDataSource() {
-//        DaoFactory.setDefaultType("jdbc");
-//
-//        HikariDataSource ds = new HikariDataSource();
-//        ds.setMaximumPoolSize(100);
-//        ds.setDriverClassName("org.postgresql.Driver");
-//        ds.setJdbcUrl("jdbc:postgresql://localhost:5432/sportfindertestdb"); ;
-//        ds.setUsername("sportfindertestdb");
-//        ds.setPassword("sportfindertestdb");
-//
-//        Flyway flyway = new Flyway();
-//        flyway.setLocations("db/migration", "db");
-//        flyway.setDataSource(ds);
-//        flyway.clean();
-//        flyway.migrate();
-//
-//        return ds;
-//    }
 
     public static DataSource getDataSource() {
         JdbcDaoFactory daoFactory = (JdbcDaoFactory) DAO_FACTORY;
