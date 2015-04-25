e1e2d8b2c9debd9180c7aed6f6635ecb7935e389
Almas Akchabayev <almas.akchabayev@gmail.com>
2015-04-26 00:16:42 +0600

    added filter that checks for static resources

--- src/main/java/com/epam/aa/sportfinder/controller/Filter.java
+++ src/main/java/com/epam/aa/sportfinder/controller/Filter.java
@@ -2,14 +2,33 @@ package com.epam.aa.sportfinder.controller;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(filterName = "Filter")
+@WebFilter(filterName = "Filter", urlPatterns = "/*", dispatcherTypes = DispatcherType.REQUEST)
 public class Filter implements javax.servlet.Filter {
     public void destroy() {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+
+//        final HttpServletRequestWrapper wrapped = new HttpServletRequestWrapper(request) {
+//            @Override
+//            public StringBuffer getRequestURL() {
+//                final StringBuffer originalUrl = ((HttpServletRequest) getRequest()).getRequestURL();
+//                return new StringBuffer("http://servername2:7001");
+//            }
+//        };
+        HttpServletRequest request = (HttpServletRequest) req;
+        String requestURI = request.getRequestURI();
+        if(!requestURI.contains("assets")) {
+            StringBuffer buffer = new StringBuffer("/controller");
+            buffer.append(requestURI);
+            request.getRequestDispatcher(buffer.toString()).forward(req, resp);
+            return;
+        }
         chain.doFilter(req, resp);
     }
 
--- src/main/java/com/epam/aa/sportfinder/controller/FrontController.java
+++ src/main/java/com/epam/aa/sportfinder/controller/FrontController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "FrontController", urlPatterns = "/pages/*")
+@WebServlet(name = "FrontController", urlPatterns = "/controller/*")
 public class FrontController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
