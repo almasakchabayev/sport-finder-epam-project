554d974fe217e3548401c32816a842e118275213
Almas Akchabayev <almas.akchabayev@gmail.com>
2015-05-06 15:50:52 +0600

    added PermissionsFIlter

---
+++ src/main/java/com/epam/aa/sportfinder/filter/PermissionsFilter.java
@@ -0,0 +1,78 @@
+package com.epam.aa.sportfinder.filter;
+
+import com.epam.aa.sportfinder.config.ControllerActionsLoader;
+import com.epam.aa.sportfinder.controller.ActionFactory;
+import com.epam.aa.sportfinder.controller.ControllerAction;
+import com.epam.aa.sportfinder.model.Manager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static com.epam.aa.sportfinder.controller.ControllerAction.*;
+
+public class PermissionsFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(PermissionsFilter.class);
+
+
+    Map<String, String[]> pathPermissions = new HashMap<>();
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        Set<Class<?>> annotated = ControllerActionsLoader.getClassesAnnotatedWithControllerAction();
+
+        for (Class<?> action : annotated) {
+            ControllerAction annotation = action.getAnnotation(ControllerAction.class);
+            String path = annotation.path();
+            String[] permissions = annotation.accessDeniedTo();
+
+            pathPermissions.put(path, permissions);
+        }
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        String path = ActionFactory.getPath(request);
+        logger.debug("permission filter checks path: {}", path);
+
+        String[] notAllowedPermissions = pathPermissions.get(path);
+        if (notAllowedPermissions != null) {
+            HttpSession session = request.getSession(false);
+
+            for (String notAllowedPermission : notAllowedPermissions) {
+                if (notAllowedPermission.equals(Permission.GUEST)) {
+                    if (session == null || session.getAttribute("user") == null) {
+                        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                    }
+                } else if (notAllowedPermission.equals(Permission.CUSTOMER)) {
+                    //todo add customer handler
+                } else if (notAllowedPermission.equals(Permission.MANAGER)) {
+                    if (session == null)
+                        continue;
+
+                    Object user = session.getAttribute("user");
+                    if (user instanceof Manager) {
+                        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                    }
+                }
+            }
+        }
+
+        chain.doFilter(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- src/main/java/com/epam/aa/sportfinder/filter/StaticUriFilter.java
+++ src/main/java/com/epam/aa/sportfinder/filter/StaticUriFilter.java
@@ -1,5 +1,8 @@
 package com.epam.aa.sportfinder.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class StaticUriFilter implements javax.servlet.Filter {
+    private static final Logger logger = LoggerFactory.getLogger(StaticUriFilter.class);
+
     public void destroy() {
     }
 
@@ -17,6 +22,7 @@ public class StaticUriFilter implements javax.servlet.Filter {
         if(!requestURI.contains("assets")) {
             StringBuffer buffer = new StringBuffer("/controller");
             buffer.append(requestURI);
+            logger.debug("request for controller action, dispatching to {}", requestURI);
             request.getRequestDispatcher(buffer.toString()).forward(req, resp);
             return;
         }
